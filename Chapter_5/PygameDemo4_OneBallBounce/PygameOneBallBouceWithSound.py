9079e5f576342c4eabaef3c06450df54e2dcf32b
Markus <mamaa003@osloskolen.no>
2022-10-21 08:29:22 +0200

    update

--- Chapter_5/PygameDemo4_OneBallBounce/PygameOneBallBouceWithSound.py
+++ Chapter_5/PygameDemo4_OneBallBounce/PygameOneBallBouceWithSound.py
@@ -10,7 +10,7 @@ import random
 BLACK = (0, 0, 0)
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
-FRAMES_PER_SECOND = 30
+FRAMES_PER_SECOND = 60
 N_PIXELS_PER_FRAME = 3
 
 # 3 - Initialize the world
@@ -19,9 +19,9 @@ window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
  
 # 4 - Load assets: image(s), sound(s),  etc.
-ballImage = pygame.image.load('images/ball.png')
-bounceSound = pygame.mixer.Sound('sounds/boing.wav')
-pygame.mixer.music.load('sounds/background.mp3')
+ballImage = pygame.image.load('Chapter_5/PygameDemo4_OneBallBounce/images/ball.png')
+bounceSound = pygame.mixer.Sound('Chapter_5/PygameDemo4_OneBallBounce/sounds/boing.wav')
+pygame.mixer.music.load('Chapter_5/PygameDemo4_OneBallBounce/sounds/background.mp3')
 pygame.mixer.music.play(-1, 0.0)
 
 
@@ -47,11 +47,26 @@ while True:
     
     # 8 - Do any "per frame" actions
     if (ballRect.left < 0) or (ballRect.right >= WINDOW_WIDTH):
-        xSpeed = -xSpeed  # reverse X direction
+        xSpeed = -xSpeed
+        if xSpeed <= 0:
+            xSpeed -=1
+
+        elif xSpeed >= 0:
+            xSpeed +=1
+          # reverse X direction
+        N_PIXELS_PER_FRAME +=1
+        print(N_PIXELS_PER_FRAME)
         bounceSound.play()
 
     if (ballRect.top < 0) or (ballRect.bottom >= WINDOW_HEIGHT):
+        N_PIXELS_PER_FRAME +=1
         ySpeed = -ySpeed  # reverse Y direction
+        if ySpeed <= 0:
+            ySpeed -=1
+
+        elif ySpeed >= 0:
+            ySpeed +=1
+        N_PIXELS_PER_FRAME +=1
         bounceSound.play()
 
     # Update the rectangle of the ball, based on the speed in two directions
